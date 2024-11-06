7396682e1c94fdb3034292655371686d96da6c6a
Nishu13 <nishanka1304@gmail.com>
2024-11-06 16:17:13 +0530

    Update exp.py
    This is generally a testing script to check the connection to server is done

--- exp.py
+++ exp.py
@@ -1,25 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Replace with your actual database URL
+
 DATABASE_URL = 'mysql+pymysql://root:password@localhost/attendance'
 
-# Create an engine
+
 engine = create_engine(DATABASE_URL)
 
-# Create a session
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
 try:
-    # Your database operations go here
+    
     connection = engine.connect()
-    # Perform database operations using the connection
+    
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
-    # Close the connection if it was opened
-    if 'connection' in locals():  # Ensure connection is defined
+    
+    if 'connection' in locals(): 
         connection.close()
 
 
